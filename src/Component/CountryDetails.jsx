5cb7fa6d34a6bfa992b2639c03ecaa3a31e012bd
shohayabt <shohayabhasan@yahoo.com>
2023-08-22 01:10:02 +0600

    Don't Know

--- src/Component/CountryDetails.jsx
+++ src/Component/CountryDetails.jsx
@@ -1,7 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Loading } from './Countries'
+import { styled } from 'styled-components'
 
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 30px;
+  padding: 0px 5%;
+`
+const FlagContainer = styled.div`
+  width: 100%;
+`
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
+const InformationContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`
+const Information = styled.p`
+  font-size: 18px;
+  text-transform: uppercase;
+  margin: 4px 0px;
+`
 export const CountryDetails = () => {
     const {name} = useParams()
     const [data, setData] = useState([])
@@ -15,12 +39,36 @@ export const CountryDetails = () => {
         }
         loadingData()
     },[])
+
    if(isLoading){
-    return <Loading><p>Loading.....</p></Loading>
+    return <Loading><Information>Loading.....</Information></Loading>
    }
   return (
     <>
-    {data[0]?.name?.common}
+    <Container>
+      <FlagContainer className="flag">
+        <Image src={data[0]?.flags?.png} alt={data[0]?.flags?.alt} />
+      </FlagContainer>
+      <div className="name"><h1>{name}</h1>
+      <InformationContainer className="country-information">
+        <div className="left-side">
+          <Information>native name: {data[0]?.name?.nativeName?.eng?.common}</Information>
+          <Information>population: {data[0]?.population}</Information>
+          <Information>region: {data[0]?.region}</Information>
+          <Information>sub region: {data[0]?.subregion}</Information>
+          <Information>capital</Information>
+        </div>
+        <div className="right-side">
+          <Information>top level domain</Information>
+          <Information>currencies</Information>
+          <Information>language</Information>
+        </div>
+      </InformationContainer>
+      <div className="footer">
+
+      </div>
+      </div>
+    </Container>
     </>
   )
 }
